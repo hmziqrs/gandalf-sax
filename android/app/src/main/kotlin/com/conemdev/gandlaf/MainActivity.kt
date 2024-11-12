b21a0c4f2aa8416a60c69c1967f283b023262617
hmziqrs <hmziqrs@gmail.com>
2024-11-12 12:54:25 +0500

    Updated flutter teamplate for android native

--- android/app/src/main/kotlin/com/conemdev/gandlaf/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.conemdev.gandlaf
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/onemdev/gandlaf/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.onemdev.gandlaf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
