330b6740fee5d593c57ad490a3448c22c1651c87
hmziqrs <hmziqrs@gmail.com>
2024-11-20 09:18:14 +0500

    Minor patch

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_performance/firebase_performance.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gandalf/configs/app.dart';
+import 'package:gandalf/firebase_options.dart';
 import 'package:video_player_media_kit/video_player_media_kit.dart';
 
 import 'Navigator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   App.showAds = false;
   VideoPlayerMediaKit.ensureInitialized(
     linux: true,
     windows: true,
+
     web: false,
-    
     android: false,
     iOS: false,
     macOS: false,
   );
 
-
+  
   runApp(ProviderScope(child: AppNavigator([])));
 }
